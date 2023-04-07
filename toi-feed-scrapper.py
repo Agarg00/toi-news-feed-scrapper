ec94bcaf7c326bff650d6bcbf0e08a6dc1e20f25
Ashish Garg <88076678+Agarg00@users.noreply.github.com>
2023-04-07 17:48:11 +0530

    Add files via upload
    Added the main py file

---
+++ toi-feed-scrapper.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+res = requests.get('https://timesofindia.indiatimes.com/home/headlines')
+soup = BeautifulSoup(res.text, 'html.parser')
+a = soup.select('.w_tle')
+
+
+# print(a[0].getText())
+# print(a[0].select('a')[0].get('href',None))
+def toi_news_headlines(links):
+    nh = []
+    for item, idx in enumerate(links):
+        title = links[item].getText()
+        href = links[item].select('a')[0].get('href', None)
+        nh.append({'title': title, 'link': f'https://timesofindia.indiatimes.com{href}'})
+    return nh
+
+
+result = toi_news_headlines(a)
+list_of_values = list(map(lambda d: d['title'], result))
+all_keys = list(map(lambda d: d['link'], result))
+
+print("Here is some news feed from TOI with titles and links:\n\n")
+for i in range(len(result)):
+    print(f'Title: \n\t{list_of_values[i]} \nLink: \n\t{all_keys[i]} \n\n')
+
+print("Total Number of Feeds : ", len(result))
